84dccc609eddeecaefd4ff3d98b2f3c6395456e9
Augustas Kniška <77825186+kniskiukas@users.noreply.github.com>
2024-03-24 16:09:35 +0200

    rough main page template with embed try out
    pridejau teksta, background ir embed pradzia, embed kazkoidel neveikia, ieskosiu ka daryt

---
+++ index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+//import "./styles.css";
+
+import App from "./App";
+export default function Square() {
+  return <button className="square">X</button>;
+}
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
